347bb4db37f1d0c773681c9b58125c8df387862d
caos <marius@sys-tem.org>
2021-12-03 23:19:37 +0100

    added automatic buildno-generation to docker-build for linux

--- docker_build.sh
+++ docker_build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run -v $(pwd):/work sysexect/ruuvi-build-env:latest fish --command='cd src && make ruuvitag_b'
+DATE=$(date +"%Y%m%d_%H%M%S")
+echo '#define BUILDNUMBER "Build '$DATE'"' > src/buildnum.h
+docker run -v $(pwd):/work sysexect/ruuvi-build-env:latest fish --command='cd src && make -j (nproc --all) ruuvitag_b'

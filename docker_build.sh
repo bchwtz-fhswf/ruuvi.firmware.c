7c0ab5d741c12bbffeffb541000a0e97e74d5ab7
caos <sysexect@gmail.com>
2021-12-04 13:50:30 +0100

    replaced ln with mv

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DATE=$(date +"%Y%m%d_%H%M%S")
 echo '#define BUILDNUMBER "Build '$DATE'"' > src/buildnum.h
-docker run -v $(pwd):/work sysexect/ruuvi-build-env:latest fish --command='ln -s /tools/nRF5_SDK_15.3.0_59ac345 /work/nRF5_SDK_15.3.0_59ac345 && cd src && make -j (nproc --all) ruuvitag_b'
+docker run -v $(pwd):/work sysexect/ruuvi-build-env:latest fish --command='mv /tools/nRF5_SDK_15.3.0_59ac345 /work/nRF5_SDK_15.3.0_59ac345 && cd src && make -j (nproc --all) ruuvitag_b'
